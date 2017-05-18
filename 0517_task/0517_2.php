14a285439cf3e2426ae02362ea53957e57dbbf2e
so-0224 <harunatuyakyuu@gmail.com>
2017-05-18 08:26:10 +0530

    Add files via upload

---
+++ 0517_task/0517_2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form  action="0517_3.php" method="get">
+      <input type="text" name="comment" value="">
+      <input type="submit" name="" value="yes">
+    </form>
+    </div>
+  </body>
+</html>
---
+++ 0517_task/0517_3.php
@@ -0,0 +1,45 @@
+<?php
+$a = $_GET["comment"];
+switch ($a) {
+  case 1: echo "January";
+          echo "<br />";
+
+  case 2:echo "February";
+          echo "<br />";
+
+  case 3:echo "March";
+          echo "<br />";
+
+  case 4:echo "April	";
+          echo "<br />";
+
+  case 5:echo "May";
+          echo "<br />";
+
+  case 6:echo "June";
+          echo "<br />";
+
+  case 7:echo "July	";
+          echo "<br />";
+
+  case 8:echo "August";
+          echo "<br />";
+
+  case 9:echo "September";
+          echo "<br />";
+
+  case 10:echo "October";
+          echo "<br />";
+
+  case 11:echo "November";
+          echo "<br />";
+
+  case 12:echo "December";
+          echo "<br />";
+          break;
+
+  default:echo 'error';
+          break;
+
+}
+ ?>
